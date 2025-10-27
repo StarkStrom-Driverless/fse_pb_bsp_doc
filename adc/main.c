741290aa53f1763965788fa35dcf449aa7e76d1a
Maximilian Hoffmann <maximilian.hoffmann@starkstrom-augsburg.de>
2025-10-27 21:26:16 +0100

    added adc

---
+++ adc/main.c
@@ -0,0 +1,69 @@
+#include "ss.h"
+
+void ss_init_error(SS_FEEDBACK feedback);
+
+uint16_t led = PIN('C', 1);
+uint16_t adc = PIN('A', 2);
+
+static void blinky_task(void *args) {
+    uint16_t value = 0;
+    struct SS_CAN_FRAME msg;
+    SS_FEEDBACK rc;
+    for (;;) {
+        ss_can_frame_set_common(&msg, 0x123, 2);
+
+        rc = ss_adc_read(adc, &value);
+
+        if (rc != SS_FEEDBACK_OK) {
+            ss_can_frame_set_signal(&msg, 15, 1, 1);
+        } else {
+            ss_can_frame_set_signal(&msg, 0, 16, value);
+        }
+
+        
+
+        ss_can_send(1, &msg);
+
+        ss_rtos_delay_ms(500);
+    }
+}
+
+int main(void)
+{
+    SS_HANDLE_INIT(ss_init());
+
+    SS_HANDLE_INIT(ss_io_init(led, SS_GPIO_MODE_OUTPUT));
+
+    SS_HANDLE_INIT(ss_adc_init(adc));
+
+    SS_HANDLE_INIT(ss_can_init(1, 1000000));
+
+    SS_HANDLE_INIT(ss_rtos_task_add(blinky_task, NULL, 1, "blinky_task"));
+
+    ss_rtos_start();
+
+    while (1) {
+
+	}
+
+    return 0;
+
+}
+
+void ss_init_error(SS_FEEDBACK feedback) {
+    uint16_t error = 0xFFFF & feedback;
+    uint16_t top_error = (feedback >> 16);
+    while(1) {
+        ss_led_error_toggle();
+        ss_delay(1000);
+    }
+}
+
+
+void vApplicationStackOverflowHook(TaskHandle_t xTask, char * pcTaskName) {
+    while(1) {
+        ss_led_error_toggle();
+        ss_delay(500);
+    }
+}
+
